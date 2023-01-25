026a06c71c54339a807915c54ddaf37a7d7e8e68
gezixi <1149994515@qq.com>
2023-01-25 23:52:45 +0800

    feature: 增加用户功能

---
+++ src/main/java/org/gezixi/regis/config/ParamConstant.java
@@ -0,0 +1,7 @@
+package org.gezixi.regis.config;
+
+public interface ParamConstant {
+
+    String USER_SESSION_NAME = "employee";
+    String DEFAULT_PASSWORD = "123456";
+}
--- src/main/java/org/gezixi/regis/controller/EmployeeController.java
+++ src/main/java/org/gezixi/regis/controller/EmployeeController.java
@@ -1,8 +1,10 @@
 package org.gezixi.regis.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import lombok.extern.slf4j.Slf4j;
 import org.gezixi.regis.common.R;
+import org.gezixi.regis.config.ParamConstant;
 import org.gezixi.regis.model.Employee;
 import org.gezixi.regis.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,8 @@ import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Slf4j
 @RestController
@@ -42,7 +46,7 @@ public class EmployeeController {
         if (emp.getStatus() == 0) {
             return R.error("账号被禁用了");
         }
-        request.getSession().setAttribute("employee", emp.getId());
+        request.getSession().setAttribute(ParamConstant.USER_SESSION_NAME, emp.getId());
         return R.success(emp);
     }
 
@@ -55,11 +59,42 @@ public class EmployeeController {
     @PostMapping("/logout")
     public R<String> logout(HttpServletRequest request) {
         // 清理session中的员工信息
-        request.getSession().removeAttribute("employee");
+        request.getSession().removeAttribute(ParamConstant.USER_SESSION_NAME);
         return R.success("退出成功");
     }
 
+    @PostMapping
+    public R<String> save(HttpServletRequest request, @RequestBody Employee employee) {
+        employee.setPassword(DigestUtils.md5DigestAsHex(ParamConstant.DEFAULT_PASSWORD.getBytes()));
+        employee.setCreateTime(LocalDateTime.now());
+        employee.setUpdateTime(LocalDateTime.now());
+        // 添加操作员工
+        Long employeeId = (Long) request.getSession().getAttribute(ParamConstant.USER_SESSION_NAME);
+        employee.setCreateUser(employeeId);
+        employee.setUpdateUser(employeeId);
+        return R.success("新增员工成功");
+    }
 
+    /**
+     * 员工信息分页查询
+     *
+     * @param page 数据总数
+     * @param pageSize 数据项大小
+     * @param name 名称
+     * @return Page对象
+     */
+    @GetMapping("/page")
+    public R<Page<Employee>> getPage(int page, int pageSize, String name) {
+        Page<Employee> pageInfo = new Page<>(page, pageSize);
+        // 构建条件构造器
+        LambdaQueryWrapper<Employee> queryWrapper = new LambdaQueryWrapper<>();
+        // 添加过滤条件
+        queryWrapper.like(Optional.ofNullable(name).isPresent(), Employee::getName, name);
+        // 添加排序条件
+        queryWrapper.orderByDesc(Employee::getUpdateTime);
+        employeeService.page(pageInfo, queryWrapper);
+        return R.success(pageInfo);
+    }
 
 
 
